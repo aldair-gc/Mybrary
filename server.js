c716cbb26b186ab9fc2d1d2b52cdfbdb73b14242
aldair-gc <98914031+aldair-gc@users.noreply.github.com>
2022-07-26 12:44:21 -0300

    first

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://aldairgc:HdvnYLXUfGcP114d@cluster0.iiwwk.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('connected to Mongoose'));
+
 app.use('/', indexRouter);
 
 
