0bfbda0c66d45a5a3a17b6b7cf56659ee0fdc21d
aldair-gc <98914031+aldair-gc@users.noreply.github.com>
2022-07-26 15:17:49 -0300

    author create

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://aldairgc:HdvnYLXUfGcP114d@cluster0.iiwwk.mongodb.net/?retryWrites=true&w=majority", {
@@ -18,6 +21,7 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 
 app.listen(process.env.PORT || 3000);
